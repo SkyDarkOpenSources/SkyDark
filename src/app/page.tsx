5f082fe293ec438cc01de5a20a378c370ef293d5
Achyut Paliwal <achyutkpaliwal@gmail.com>
2025-05-26 14:08:49 +0300

    2nd commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,21 @@ export default function Home(){
   return(
     <div className="flex flex-col p-4 font-mono">
       <main className="h-screen place-content-center">
-        <div className="flex flex-col justify-center place-items-center">
-          <div className="flex flex-col justify-center place-items-center text-4xl">
-            <img src="vercel.svg" alt="" />
+        <div className="flex flex-col file:justify-center place-items-center">
+          <div className="justify-center place-items-center text-4xl border">
+            <img src="SkyDarkLogo.png" alt="" 
+            className="h-72 w-72 rounded-full object-cover mx-auto"
+            />
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col border p-10 justify-center place-items-center gap-3">
             <div className="flex flex-row justify-center place-items-center gap-4 text-5xl text-teal-300">
               <TextScramble
                 text="Welcome to SkyDark"
                 className=""
               />
+            </div>
+            <div>
+              <p>A space-tech company that aims to build model-rockets, drones and aircrafts</p>
             </div>
           </div>
         </div>
