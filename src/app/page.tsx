afeab6303a96105dcca9e5808f3d2b340e5ea2c2
SkyDark <skydarkteams@gmail.com>
2025-06-01 22:08:52 +0300

    13th commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import TextScramble from "@/components/TextScramble/page";
+import React from 'react';
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center justify-center text-7xl text-cyan-700 font-light">
-        <TextScramble
-          text="Welcome to SkyDark"
-        />
-      </div>
     </main>
   );
 }  
