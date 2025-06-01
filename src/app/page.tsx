2dbf2c16386f60b53a4f075b67acad3488080ff5
SkyDark <skydarkteams@gmail.com>
2025-06-02 02:01:18 +0300

    16th commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-full p-4"> 
       <main className="flex flex-col h-auto items-center justify-between p-24">
         <div className="flex flex-col items-center justify-center h-96 w-96 mb-10">
-          <img src="SkyDark.png" alt="" />
+          <Image 
+           src="/SkyDark.png"
+           alt="SkyDark Logo"
+           width={200}
+           height={200}
+          />
         </div>
         <div className="text-3xl font-mono text-gray-200 lg:text-6xl md:text-4xlb">  
            Welcome to SkyDark
