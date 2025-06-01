b43d96ee71abc94d1f668cd9b149ceb5814639fb
SkyDark <skydarkteams@gmail.com>
2025-06-01 20:36:26 +0300

    2nd commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center justify-center text-7xl text-cyan-700 font-light">
-        
         <TextScramble
           text="Welcome to SkyDark"
         />
