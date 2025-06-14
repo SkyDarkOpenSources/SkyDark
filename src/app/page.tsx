517d6011989824235503ca15d7cf6a3b277f5119
Achyut Paliwal <achyutkpaliwal@gmail.com>
2025-06-14 04:20:27 +0300

    31st commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
-import Image from "next/image";
-import { Roboto } from 'next/font/google'
- 
-const roboto = Roboto({
-  weight: '400',
-  subsets: ['latin'],
-})
+
 
 export default function Menubar(){
     return(
