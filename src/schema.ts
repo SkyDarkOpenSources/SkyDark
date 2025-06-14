4f3694023651a6ea799caf62104ed96a5019f60c
Achyut Paliwal <achyutkpaliwal@gmail.com>
2025-06-14 18:35:37 +0300

    35th commit

--- src/schema.ts
+++
@@ -1,6 +0,0 @@
-import { pgTable, text } from 'drizzle-orm/pg-core'
-
-export const Users = pgTable('Users', {
-  user_id: text('user_id').primaryKey().notNull(),
-  email: text('email').notNull(),
-})
