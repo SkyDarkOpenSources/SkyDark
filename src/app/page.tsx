4d5d75db659564559f8e4d8eabe60c01bae8c0d8
SkyDark <skydarkteams@gmail.com>
2025-06-22 18:36:50 +0300

    43rd commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home(){
     <div className="p-10 flex flex-col">
       <main className="flex flex-col items-center h-screen">
         <div className="flex justify-center">
-          <span className="text-3xl">Hello </span>
         </div>
         <div></div>
       </main>
--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import {
   SignInButton,
+  SignUpButton,
+  SignedOut,
   SignedIn,
   UserButton,
   useUser,
@@ -9,39 +11,6 @@ import { FaBars } from "react-icons/fa";
 
 export default function Navbar() {
 
-    const { isSignedIn } = useUser();
-
-    if (!isSignedIn){
-        return(
-        <nav className="p-5 flex flex-row-3 justify-between bg-transparent backdrop:blur-lg ">
-            <div className="text-3xl font-semi-bold text-slate-100 font-sans">
-                SkyDark
-            </div>
-            <div className="flex flex-rows-2 text-xl font-sans gap-2 text-gray-100">
-                <div className="flex flex-row-6 lg:visible md:visible gap-4">
-                    <div className="lg:visible md:visible invisible flex justify-center items-center">
-                        <a href="">Home</a>
-                    </div>
-                    <div className="lg:visible md:visible invisible flex justify-center items-center">
-                        <a href="">About</a>
-                    </div>
-                    <div className="lg:visible md:visible invisible flex justify-center items-center">
-                        <a href="">Donate</a>
-                    </div>
-                    <div className="lg:visible md:visible invisible flex justify-center items-center">
-                        <SignInButton />
-                    </div>
-                </div>
-                <div className="lg:hidden md:hidden visible"> 
-                    <a href="">
-                        <FaBars className="text-3xl"
-                        />
-                    </a>
-                </div>   
-            </div>
-        </nav>
-        );
-    }
 
     return (
         <nav className="p-5 flex flex-row-3 justify-between bg-transparent backdrop:blur-lg ">
@@ -59,9 +28,13 @@ export default function Navbar() {
                     <div className="lg:visible md:visible invisible flex justify-center items-center">
                         <a href="">Donate</a>
                     </div>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton></SignInButton>
+                        <SignUpButton></SignUpButton>
+                    </SignedOut>
                 </div>
                 <div className="lg:hidden md:hidden visible"> 
                     <a href="">
