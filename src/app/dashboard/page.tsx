aa640952f861b2a5a611c40db521426aabdcad40
SkyDark <skydarkteams@gmail.com>
2025-06-28 22:04:00 +0300

    71st commit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold mb-4">Welcome {user?.username}</h1>
+      <h1 className="text-2xl font-bold mb-4">Welcome {user?.username}.</h1>
       {user ? (
         <p className="text-lg">Hello, {user.username}!</p>
       ) : (
